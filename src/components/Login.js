82b9a29ea930db74aa0ef7b68ad12dc35a9f6dcf
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-22 16:06:37 +0300

    feat: added login, sign up and not found components

---
+++ src/components/Login.js
@@ -0,0 +1,125 @@
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import YupPassword from 'yup-password';
+import Button from '../layout/Button';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import { TailSpin} from 'react-loader-spinner'
+import { useState } from 'react';
+function Login({ setUser }) {
+  YupPassword(Yup);
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  function handleSignUpClick() {
+    navigate('/sign-up');
+  }
+
+  const errorMessagesSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email cannot be blank'),
+    password: Yup.string().password().min(8).required('Password cannot be blank'),
+  });
+
+  return (
+    <>
+      {
+        loading && (
+          <div
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              background: 'rgba(255,255,255, 0.5)',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              zIndex: 999,
+            }}
+          >
+            <TailSpin 
+                  height={100}
+                  width={100}
+                  radius={10}
+                  color="#000000"
+                  ariaLabel='tail-spin-loading'
+                  visible
+                />
+          </div>
+        )
+      }
+      <div className='flex flex-col items-center justify-center p-20'>
+        <h1 className='font-bold text-2xl mb-8'> Log In</h1>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={errorMessagesSchema}
+          onSubmit={(values, formikBag) => {
+            // formikBag.setSubmitting(true);
+            // setLoading(true)
+            // fetch('https://game-haven-backend.onrender.com/login', {
+            //   method: 'POST',
+            //   headers: {
+            //     'Content-Type': 'application/json',
+            //   },
+            //   credentials: 'include',
+            //   body: JSON.stringify(values),
+            // })
+            //   .then((res) => {
+            //     if (res.ok) {
+            //       enqueueSnackbar('Logged in successfully', { variant: 'success' });
+            //       return res.json();
+            //     } else if (res.status === 401) {
+            //       enqueueSnackbar('Invalid email or password', { variant: 'error' });
+            //       return null;
+            //     }
+            //   })
+            //   .then((data) => {
+            //     if (data) {
+            //       setUser(data);
+            //       console.log(data);
+            //       navigate('/all-games');
+            //     }
+            //   })
+            //   .catch((e) => console.log(e))
+            //   .finally(() => {
+            //     setLoading(false)
+            //     formikBag.resetForm();
+            //   });
+            enqueueSnackbar("Thanks for trying out our platform!", {variant: 'success'})
+            setLoading(false)
+          }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form className='flex flex-col content-center justify-center max-w-xs w-full'>
+              <label className='m-2 font-bold' htmlFor='email'>
+                Email address
+              </label>
+              <Field type='text' name='email' id='email' className='text-rich-black px-2 py-1 rounded' />
+              {touched.email && errors.email && <div className='text-indian-red'>{errors.email}</div>}
+
+              <label className='m-2 font-bold' htmlFor='password'>
+                Password
+              </label>
+              <Field type='password' name='password' id='password' className='text-rich-black px-2 py-1 rounded' />
+              {touched.password && errors.password && <div className='text-indian-red'>{errors.password}</div>}
+
+              <Button type='submit' content='Log In' className='text-sm my-5 mx-auto py-2 w-2/6' disabled={isSubmitting} />
+            </Form>
+          )}
+        </Formik>
+        <p>
+          Don't have an account? <span className='font-bold' onClick={handleSignUpClick}>
+            Sign Up
+          </span>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/components/Signup.js
@@ -0,0 +1,144 @@
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
+import YupPassword from 'yup-password'
+import Button from '../layout/Button';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useSnackbar } from 'notistack';
+import { TailSpin } from 'react-loader-spinner';
+import supabase from '../config/supabaseClient';
+
+
+
+
+function SignUp() {
+    const [refreshPage, setRefreshPage] = useState(false)
+    const navigate = useNavigate();
+    const {enqueueSnackbar} = useSnackbar();
+    const [loading, setLoading] = useState(false);
+
+    function handleLogInClick(){
+        navigate('/login')
+    }
+
+
+
+    YupPassword(Yup)
+    const errorMessagesSchema = Yup.object().shape({
+        username: Yup.string()
+            .min(2, "Username too short!")
+            .max(50, "Username too long")
+            .required('This field is required'),
+        email: Yup.string()
+            .email('Invalid email')
+            .required('This field is required'),
+        password: Yup.string()
+            .password()
+            .required("Password cannot be blank")
+            .min(8),
+        confirmPassword: Yup.string()
+            .required('Please retype your password')
+            .oneOf([Yup.ref('password')], "Passwords do not match!")
+    });
+
+    
+  return (
+    <>
+        {loading && (
+            <div
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    background: 'rgba(255, 255, 255, 0.5)',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: 999
+                }}    
+            >
+                <TailSpin
+                    height={100}
+                    width={100}
+                    radius={5}
+                    color='#000000'
+                    ariaLabel='tail-spin-loading'
+                    visible
+                />
+            </div>
+        )}
+        <div className='flex flex-col items-center justify-center p-20'>
+            <h1 className='font-bold text-2xl mb-8'> Sign Up</h1>
+            <Formik
+                initialValues={{
+                    username: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                }}  
+                validationSchema={errorMessagesSchema}  
+                onSubmit={({confirmPassword, e, ...values})=>{
+                    // setLoading(true)
+                    // fetch("https://game-haven-backend.onrender.com/signup",{
+                    //     method: "POST",
+                    //     headers: {
+                    //         "Content-Type": "application/json",
+                    //     },
+                    //     body: JSON.stringify(values, null, 2),
+                    // }).then((res)=>{
+                    //     if (res.status === 201){
+                    //         navigate('/login');
+                    //         setRefreshPage(!refreshPage);
+                    //         enqueueSnackbar("Account created successfully", {variant: "success"})
+                    //     }
+                    //     else{
+                    //         enqueueSnackbar("Email or username exists", {variant: 'error'})
+                    //     }
+                    // })
+                    // .finally(()=>{
+                    //     setLoading(false);
+                    // })
+                    // e.resetForm();
+                    enqueueSnackbar("Thanks for trying out our platform!", {variant: 'success'})
+                }}
+            >
+                {({errors, touched})=>(
+                    <Form className='flex flex-col content-center justify-center max-w-xs w-full'>
+                        <label className='m-2 font-bold' htmlFor='username'>
+                            Username
+                        </label>
+                        <Field type='text' name="username" id='username' className='text-rich-black px-2 py-1 rounded'/>
+                        {touched.username && errors.username && <div className='text-indian-red'>{errors.username}</div>}
+
+                        <label className='m-2 font-bold' htmlFor='email'>
+                            Email address
+                        </label>
+                        <Field type='text' name="email" id='email' className='text-rich-black px-2 py-1 rounded'/>
+                        {touched.email && errors.email && <div className='text-indian-red'>{errors.email}</div>}
+
+                        <label className='m-2 font-bold' htmlFor='password'>
+                            Password
+                        </label>
+                        <Field type='password' name='password' id='password' className='text-rich-black px-2 py-1 rounded' />
+                        {touched.password && errors.password && <div className='text-indian-red'>{errors.password}</div>}
+
+                        <label className='m-2 font-bold' htmlFor='password'>
+                            Confirm Password
+                        </label>
+                        <Field type='password' name='confirmPassword' id='confirmPassword' className='text-rich-black px-2 py-1 rounded' />
+                        {touched.confirmPassword && errors.confirmPassword && <div className='text-indian-red'>{errors.confirmPassword}</div>}
+
+                        <Button type='submit' content='Sign Up' className='text-sm my-5 mx-auto px-1 py-2 w-2/6'/>
+                    </Form>
+                )}
+
+            </Formik>
+            <p>Already Registered? <span className='font-bold' onClick={handleLogInClick}>Log In Here</span></p>
+        </div>
+    </>
+  )
+}
+
+export default SignUp
